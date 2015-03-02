9e1cdf32199c4893ac7291a5d2587730698f34da
Sony <syam.s@outlook.com>
2015-03-02 09:16:03 +0530

    test

---
+++ src/com/example/Logging/LoggingClass.java
@@ -0,0 +1,24 @@
+
+package com.example.Logging;
+
+import org.testng.annotations.Test;
+
+public class LoggingClass {
+
+	@Test(priority = 0)
+	public void methodAddingNumbers() {
+		System.out.println("Helloo.. Im in method adding numbers");
+	}
+
+	@Test(priority = 1)
+	public void dividedByZero() {
+		System.out.println("Helloo.. Im in method divided by zero");
+		int e = 1 / 0;
+	}
+
+	@Test(dependsOnMethods = { "dividedByZero" })
+	public void methodSkip() {
+		System.out.println("Helloo.. Im in method skip");
+	}
+
+}
